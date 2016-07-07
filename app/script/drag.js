32a7aea1abab143b6f456354627bfb14e669e74e
mamoru_kijima <mamoru.kijima@dena.jp>
2016-07-07 14:51:17 +0900

    drag.jsをリファクタリング

--- app/script/drag.js
+++ app/script/drag.js
@@ -1,50 +1,57 @@
+ // =============================================================
+ // drag.js - リスト要素のドラッグ&ドロップに関係する処理を行う
+ // =============================================================
+
+// 利用モジュール ================================================
+import $ from 'jquery';               // jqueryモジュール
+require('jquery-ui');                 // jquery UIを読み込み
+import * as storage from './storage'; // ローカルストレージを操作する関数をインポート
+// ============================================================
+
+// グローバル変数 =================================================
+let listAreaId = null; // li要素をドロップする要素
+// ============================================================
+
 /**
- * リスト要素のドラッグ操作に関係するファイル
+ * dragEvent - li要素にドラッグイベントを設定する関数
  */
-
-import $ from 'jquery'; // jqueryモジュール
-require('jquery-ui'); // jquery UIを読み込み
-import * as storage from './storage'; // modelを操作する関数
-
-var listAreaId = null; // 要素をドロップする先の変数
-const moveDecision = true; // 動かす判定
-
-// リスト要素にドラッグイベントを設定する関数
 export function dragEvent() {
-  // $('.c-sticky').addClass('list-position');
   $('.c-sticky').draggable({
     snap: '.p-task-listArea', // スナップさせる領域
-    snapMode: 'inner', // スナップ適応
-    opacity: 0.4, // ドラッグ時の透明度
-    revert: 'invalid', // 指定領域以外はスナップできない
+    snapMode: 'inner',        // スナップの判定
+    opacity: 0.4,             // ドラッグ時の透明度
+    revert: 'invalid',        // 指定領域以外はスナップできないに
 
-    // ドラッグ操作を開始した時に呼び出される----------------
-    start: function(evt, ui) {
-    },
-    // ドラッグ操作を終了した時に呼び出される----------------
+    // ドラッグ操作を終了時に呼び出される関数
     stop: function(evt, ui) {
-      const textDataArray = JSON.parse(localStorage.getItem('data')); // 動かした時に配列を取得
-      const listItemText = ($.trim($(this).text())); // 先頭と末尾から空白を削除して格納
-      // console.log(listAreaId);
+      const textDataArray = JSON.parse(localStorage.getItem('data'));
+      // 先頭と末尾から空白を削除して格納
+      const listItemText = ($.trim($(this).text()));
+      // li要素のテキストが配列内の要素にマッチするまで繰り返し
       for (let i = 0; i < textDataArray.length; i++) {
+        // もしマッチしたら処理を行う
         if (textDataArray[i].text === listItemText) {
+          // マッチした要素の配列位置
           const iPos = i;
+          // 要素のkey(category:)を上書きする
           textDataArray[iPos].category = listAreaId;
-          // console.log(listAreaId);
-          // console.log(textDataArray[iPos].category);
-          // console.log(textDataArray);
+          // ストレージを更新
           storage.sendStorage(textDataArray);
         }
       }
     },
-    // -----------------------------------------------
+    // stop: function終了
   });
 }
 
+// ============================================================
+
 // 要素をドロップした後に呼ばれる関数
 $('.p-task-listArea').droppable({
-  accept: '.c-sticky', // 受け入れる要素
+  // 受け入れる要素
+  accept: '.c-sticky',
   drop: function(evt, ui) {
-    listAreaId = $(this).attr('id'); //受け入れ先のIDを取得
-  }
+   // スナップさせる領域のid名を取得
+    listAreaId = $(this).attr('id');
+  },
 });
