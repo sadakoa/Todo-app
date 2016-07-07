15f01cbdd223354915f380f3cca40cec5132468f
mamoru_kijima <mamoru.kijima@dena.jp>
2016-07-07 17:41:09 +0900

    view.jsをリファクタリング

--- app/script/view.js
+++ app/script/view.js
@@ -1,29 +1,27 @@
-/**
- * design - View
- * DOM操作に関係するファイル
- */
+// =============================================================
+// view.js - DOM要素に関係する処理を行う
+// =============================================================
 
-import $ from 'jquery'; // jqueryモジュール
-import * as storage from './storage'; // modelを操作する関数
-import * as modal from './modal'; // モーダル関数
-import * as log from './log' // タスク履歴を保持する関数
-import * as drag from './drag'; // ドラッグ操作する関数
+// 利用モジュール ================================================
+import $ from 'jquery';               // jqueryをインポート
+import * as storage from './storage'; // ローカルストレージを操作する関数
+import * as modal from './modal';     // モーダルに関する関数
+import * as log from './log';         // タスクログに関する関数
+import * as drag from './drag';       // ドラッグ操作に関係する関数
+// ============================================================
 
-// ========================================================================
-// リスト要素を追加していく変数 (本番環境でletだと要素が定義されてないERRORが出るのでvarに変更)
-var textDataArray = JSON.parse(localStorage.getItem('data'));
-
+// li要素を追加していく配列
+let textDataArray = JSON.parse(localStorage.getItem('data'));
 // もしローカルストレージのデータが空なら初期化
 if (textDataArray === null) {
   textDataArray = [];
 }
-// ========================================================================
-
+// ============================================================
 
 /**
- * addListItem - カードに作成したリストを追加する関数
+ * addListItem - li要素を指定領域へ追加する関数
  */
-export function addListItem() {
+export const addListItem = () => {
   // inputに記入された文字を変数に格納
   const inputValue = $('.new-task-input').val();
   // 選択されたoptionを変数に格納
@@ -32,56 +30,44 @@ export function addListItem() {
     <li class="c-sticky p-task-listarea__item">${inputValue}<a class="p-task-edit">
     <img src="images/edit.png" width="20" class="p-task-edit__img"></a></li>
   `);
-
   // 生成するリストエリアのIDを取得
-  const listArea = ('#' + selectValue);
+  const listArea = $(`#${selectValue}`);
   // タスクをカードに追加
-  $(listArea).append(listItem);
-
+  listArea.append(listItem);
   // 配列にデータを追加
   textDataArray.push({
     text: inputValue, category: selectValue,
   });
-  console.log(textDataArray);
-
-  // 配列のデータをストレージに送る
-  storage.sendStorage(textDataArray);
-
-  // モーダルを削除
-  $('.c-modal').remove();
-
   // 各リストの編集ボタンを変数に格納
   const EditTaskButton = $(listItem).find('.p-task-edit');
-
   // イベント関数の初期化
   EditTaskButton.on('click', function set() {
     const [editEl, listItemText] = [$(this), $(this).parent().text()];
     modal.openEditModal(editEl, listItem, listItemText);
   });
-
+  // 配列のデータをストレージに送る
+  storage.sendStorage(textDataArray);
   // タスクの履歴を保持する関数
   log.getTaskLog(inputValue);
-
   // サイドバーにタスク名前を生成する関数
   log.renderTaskLog(inputValue);
-
   // ドラッグ&ドロップイベントの追加
   drag.dragEvent();
-}
+  // モーダルを削除
+  $('.c-modal').remove();
+};
 
 // ========================================================================
-
 
 /**
  * removeAllListItem - 全てのリストを削除する関数
  */
-export function removeAllListItem() {
+export const removeAllListItem = () => {
   $('.p-task-listArea').empty();
   storage.removeAllStorage();
-}
+};
 
 // =========================================================================
-
 
 /**
  * renderlistItem - li要素を生成する関数
@@ -89,13 +75,13 @@ export function removeAllListItem() {
  * @param  {type} storageData 配列データ
  * @param  {type} i           配列のlength用カウンタ
  */
-export function renderlistItem(storageData, i) {
+export const renderlistItem = (storageData, i) => {
   const listItem = $(`
     <li class="c-sticky p-task-listarea__item">${storageData[i].text}<a class="p-task-edit">
     <img src="images/edit.png" width="20" class="p-task-edit__img"></a></li>
   `);
   return listItem;
-}
+};
 
 // ========================================================================
 
@@ -105,12 +91,11 @@ export function renderlistItem(storageData, i) {
  * @param  {type} aListItem     各リスト要素
  * @param  {type} aListItemText 各リストのテキスト要素
  */
-export function removeListItem(aListItem, aListItemText) {
+export const removeListItem = (aListItem, aListItemText) => {
   aListItem.remove();
   $('.c-edit-modal').remove();
   $('.opacity-modal').remove();
 
-  // 配列の中身の数だけ処理を繰り返す
   for (let i = 0; i < textDataArray.length; i++) {
     // もし配列のtextキーに当てはまる要素があったら
     if (textDataArray[i].text === $.trim(aListItemText)) {
@@ -121,7 +106,7 @@ export function removeListItem(aListItem, aListItemText) {
   }
   // ストレージから要素を削除
   storage.removeStorage(textDataArray);
-}
+};
 
 // ========================================================================
 
@@ -132,7 +117,7 @@ export function removeListItem(aListItem, aListItemText) {
  * @param  {type} aInputEl    インプット要素
  * @param  {type} aTaskEditEl 各リストの編集要素
  */
-function AgainSetEdit(aListItem, aInputEl, aTaskEditEl) {
+const AgainSetEdit = (aListItem, aInputEl, aTaskEditEl) => {
   // 保存ボタンがクリックされたら要素の中身を変更、ストレージを修正する
   // インプットに入力された値を格納
   const newTextData = aInputEl.val();
@@ -147,9 +132,9 @@ function AgainSetEdit(aListItem, aInputEl, aTaskEditEl) {
     modal.openEditModal(editEl, aListItem, listItemText);
   });
   return newTextData;
-}
-// ========================================================================
+};
 
+// ========================================================================
 
 /**
  * cancelEdit - リストの編集をキャンセルする関数
@@ -158,7 +143,7 @@ function AgainSetEdit(aListItem, aInputEl, aTaskEditEl) {
  * @param  {type} aListItemText 各リストのテキスト要素
  * @param  {type} aTaskEditEl   各リストの編集要素
  */
-function cancelEdit(aListItem, aListItemText, aTaskEditEl) {
+const cancelEdit = (aListItem, aListItemText, aTaskEditEl) => {
   // リストの中身を空にして要素を再生成する
   aListItem.empty().text(aListItemText).append(aTaskEditEl);
   // 各リストの編集ボタンを変数に格納
@@ -168,7 +153,7 @@ function cancelEdit(aListItem, aListItemText, aTaskEditEl) {
     const [editEl, listItemText] = [$(this), $(this).parent().text()];
     modal.openEditModal(editEl, aListItem, listItemText);
   });
-}
+};
 
 // ========================================================================
 
@@ -179,7 +164,7 @@ function cancelEdit(aListItem, aListItemText, aTaskEditEl) {
  * @param  {type} aListItemText 古いリスト要素のテキスト
  * @param  {type} newTextData   新しいリスト要素のテキスト
  */
-function putArrayData(aListItem, aListItemText, newTextData) {
+const putArrayData = (aListItem, aListItemText, newTextData) => {
   for (let i = 0; i < textDataArray.length; i++) {
     // もし配列のtextキーに当てはまる要素があったら
     if (textDataArray[i].text === $.trim(aListItemText)) {
@@ -193,19 +178,17 @@ function putArrayData(aListItem, aListItemText, newTextData) {
 
       // リストが所属するカードのカテゴリ
       const selectValue = aListItem.parent().attr('id');
-      console.log(selectValue); // 確認
       // 削除した位置に新規で配列にデータを追加
       textDataArray.splice(iPos, 0, {
         text: newTextData, category: selectValue,
       });
-      console.log(textDataArray);
 
       // ストレージデータを更新
       storage.sendStorage(textDataArray);
       break;
     }
   }
-}
+};
 
 // ========================================================================
 
@@ -214,7 +197,7 @@ function putArrayData(aListItem, aListItemText, newTextData) {
  * @param  {type} aListItem 各リスト要素
  * @param  {type} aListItemText 各リストのテキスト要素
  */
-export function editListItem(aListItem, aListItemText) {
+export const editListItem = (aListItem, aListItemText) => {
   modal.closeEditModal();
   // テキスト要素を変数に格納
   const textData = aListItemText;
@@ -251,6 +234,6 @@ export function editListItem(aListItem, aListItemText) {
     // 要素の編集をキャンセルする関数
     cancelEdit(aListItem, aListItemText, taskEditEl);
   });
-}
+};
 
 // ========================================================================
